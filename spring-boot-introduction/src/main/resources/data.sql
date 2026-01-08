a38190f1acfc4f8b1381de8c8b77079854f77d6b
Yuta_Fuseki <124555669+yuta10200831@users.noreply.github.com>
2026-01-08 17:58:40 +0900

    Add files via upload

---
+++ spring-boot-introduction/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tasks (summary, description, status) VALUES ('Spring Boot を学ぶ', 'TODO アプリを作る', 'DONE');
+INSERT INTO tasks (summary, description, status) VALUES ('Spring Security を学ぶ', 'ログイン機能を作る', 'TODO');
